9651fa176bae93fcb8e19c4b4c6c8d4f5c711b02
AMD OSRC (AMD Operating System Research Center) <tapper@tapper-testing.org>
2012-07-05 11:35:00 +0000

    Import of AMD/Tapper-Base-4.0.5 from CPAN.
    gitpan-cpan-distribution: Tapper-Base
    gitpan-cpan-version:      4.0.5
    gitpan-cpan-path:         AMD/Tapper-Base-4.0.5.tar.gz
    gitpan-cpan-author:       AMD
    gitpan-cpan-maturity:     released

--- lib/Tapper/Base.pm
+++ lib/Tapper/Base.pm
@@ -1,9 +1,11 @@
 package Tapper::Base;
+# git description: v4.0.4-2-g93e0db7
+
 BEGIN {
   $Tapper::Base::AUTHORITY = 'cpan:AMD';
 }
 {
-  $Tapper::Base::VERSION = '4.0.4';
+  $Tapper::Base::VERSION = '4.0.5';
 }
 # ABSTRACT: Tapper - Common functions for all Tapper classes
 
